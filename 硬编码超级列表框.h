82e97a8561b0112f16f3d9b67d03dca32f5c8d1a
627777359@qq.com <627777359@qq.com>
2024-07-04 11:55:11 +0800

    添加项目文件。

---
+++ 硬编码超级列表框.h
@@ -0,0 +1,32 @@
+﻿
+// 硬编码超级列表框.h: PROJECT_NAME 应用程序的主头文件
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "在包含此文件之前包含 'pch.h' 以生成 PCH"
+#endif
+
+#include "resource.h"		// 主符号
+
+
+// C硬编码超级列表框App:
+// 有关此类的实现，请参阅 硬编码超级列表框.cpp
+//
+
+class C硬编码超级列表框App : public CWinApp
+{
+public:
+	C硬编码超级列表框App();
+
+// 重写
+public:
+	virtual BOOL InitInstance();
+
+// 实现
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern C硬编码超级列表框App theApp;
---
+++ 硬编码超级列表框Dlg.h
@@ -0,0 +1,39 @@
+﻿
+// 硬编码超级列表框Dlg.h: 头文件
+//
+
+#pragma once
+
+
+// C硬编码超级列表框Dlg 对话框
+class C硬编码超级列表框Dlg : public CDialogEx
+{
+// 构造
+public:
+	C硬编码超级列表框Dlg(CWnd* pParent = nullptr);	// 标准构造函数
+
+// 对话框数据
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_MY_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 支持
+
+
+// 实现
+protected:
+	HICON m_hIcon;
+
+	// 生成的消息映射函数
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	CListCtrl list1;
+	CEdit edit1;
+	afx_msg void OnBnClickedOk();
+	afx_msg int OnCreate(LPCREATESTRUCT lpCreateStruct);
+};
